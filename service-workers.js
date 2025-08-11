d12016596464b25d88d9cd2fd25fb6c8d7b2a522
nrapasi88 <nrapasi@gmail.com>
2025-08-10 23:35:33 -0400

    Create service-workers.js

---
+++ service-workers.js
@@ -0,0 +1,28 @@
+// Basic offline cache for GitHub Pages
+const CACHE_VERSION = 'sales-studio-v1';
+const ASSETS = [
+  './',            // root
+  './index.html',  // main app
+  './manifest.json'
+  // You can list images/fonts if you add some later
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
